7310486556d57b6bc69ef0e30d5774d866d71f13
Rodrigo Bossini <professorbossini@gmail.com>
2022-02-25 17:26:20 -0300

    Retomar da secao 1.5 da apostila 02, falando sobre vetores

---
+++ aula.js
@@ -0,0 +1,78 @@
+
+
+// const a = []
+// const b = a
+// console.log (a === b)
+
+// Thread.stop()
+
+// console.log ([] === [])
+
+// console.log (1 == [true])
+
+// console.log (1 == [1, 1])
+
+// console.log ([] == false)
+
+// console.log (null == undefined)
+
+// console.log (null == null)
+
+//console.log(1 == [1])
+
+// console.log (true == 1)
+
+
+// console.log (1 == 1)
+// console.log (1 == "1")
+// console.log (1 === 1)
+// console.log (1 === "1")
+
+
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + n2
+// // console.log(n3)
+
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+
+
+// var idade = 18
+// console.log ("Oi, " + nome)
+// if (idade >= 18){
+//     //içamento hoist
+//     var nome = "José"
+//     console.log ("Parabéns, " + nome + ", você pode dirigir")
+// }
+// console.log ("Até, " + nome)
+
+// var linguagem = "Java"
+// console.log (`Aprendendo ` + linguagem)
+// var linguagem = "Javascript"
+// // printf ("Aprendendo %s", linguagem)
+// console.log (`Aprendendo ${linguagem}`)
+// int a;
+// int a;
+
+
+// const nome = "José"
+// // nome = "Ana"
+// // const n
+// const endereco = 'Rua K, 10'
+// const sobrenome = `Silva`
+
+
+// let a = 2
+// let b = "abc"
+// a = true
+// a = "abc"
+
+// var c = 2 + 3
+// var d = "abcd"
+
+
+
+
+
