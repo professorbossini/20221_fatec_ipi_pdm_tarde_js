b4fc0becaca19c482fc3e37d88eee6c54a7cdae6
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-11 17:07:31 -0300

    Terminamos o bloco de codigo 3.3.3.3. Retomar fazendo requisicoes junto ao servico de previsao do tempo

--- aula.js
+++ aula.js
@@ -1,6 +1,98 @@
-const nome = "João"
-const idade = 17
-const objeto = { nome, idade }
+function calculoRapidinho (n){
+    return Promise.resolve((n * (n + 1)) / 2)
+}
+
+function f (n){
+    return new Promise ((resolve, reject) => {
+        resolve ((n * (n + 1)) / 2)
+    })
+}
+
+calculoRapidinho(10).then (res => console.log (res))
+
+
+
+
+// //1 + 2 + ... + n-1 + n
+// function calculoDemorado (n){
+//     let p = new Promise ((resolve, reject) => {
+//         if (n < 0){
+//             reject('Somente valores positivos')
+//         }
+//         else{
+//             let res = 0
+//             for (let i = 1; i <= n; i++)
+//                 res += i
+//             resolve(res)
+//         }
+//     })
+//     return p
+// }
+
+// calculoDemorado(-10)                
+// .then ((res) => {
+//     console.log ("OK, deu certo: " + res)
+// })
+// .catch (erro => {
+//     console.log ("Falhou: " + erro)
+// })
+
+// const resultadoFuturo = calculoDemorado(10)
+// resultadoFuturo.then ((res) => console.log(res))
+
+// function exemplo (){
+//     let p = new Promise(function(resolve, reject){
+//         resolve(1)
+//     })
+//     return p
+// }
+
+// const resultadoFuturo = exemplo()
+// resultadoFuturo.then((resultado) => console.log(resultado))
+
+
+// function exemplo (){
+//     return 1
+// }
+
+// const resultado = exemplo()
+// console.log(resultado)
+
+
+// const fs = require ('fs')
+// const abrirArquivo = function (nomeArquivo){
+//     const exibirConteudo = function (erro, conteudo){
+//         if (erro){
+//             console.log ("Deu erro: " + erro)
+//         }        
+//         else{
+//             console.log (conteudo.toString())
+//             //processamento
+//             const dobro = 2 * +conteudo.toString()
+//             const finalizar = function (erro){
+//                 if (erro){
+//                     console.log ("Deu erro: " + erro)
+//                 }
+//                 else{
+//                     console.log ("Salvou o dobro com sucesso")
+//                 }
+//             }
+//             fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+
+//         }
+//     }
+//     //segundo parâmetro: função callback
+//     fs.readFile(nomeArquivo, exibirConteudo)
+// }
+
+// abrirArquivo('arquivo.txt')
+
+//IO-Bound
+//CPU-Bound
+
+// const nome = "João"
+// const idade = 17
+// const objeto = { nome, idade }
 // let calc = {
 //     soma: (a, b) => a + b,
 //     subtracao: function (a, b){
